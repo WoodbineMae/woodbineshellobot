c9e7f5629c9f30166e89981939290d4e54e0346a
kara "woodbine" fraine <woodbine@gmail.com>
2021-01-18 19:16:22 -0600

    changesasofJan182020

---
+++ woodbineshellobot/ViewControllerTwo.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerTwo.swift
+//  woodbineshellobot
+//
+//  Created by Kara Fraine on 1/18/21.
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+    @IBOutlet weak var goodbyeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
